26fb0646d4566ca6bcb5d10439409839d270c6db
Deividas Norkunas <deividas.norkunas@kilo.health>
2023-12-17 00:01:19 +0200

    prompt format

--- lib/generate-prompt.ts
+++ lib/generate-prompt.ts
@@ -14,17 +14,40 @@ export function generatePrompt(values: FormData): string {
 - The recipe must be for ${values.people} people;
 - Difficulty of execution: ${values.difficulty} ;
     
-Example with ingredients: Mince, Mushroom, Pasta:
+Example with ingredients: Cheese, Avocado, Pepper:
 
-Mince and Mushroom Pasta
+Cheese and Avocado Sandwich
+
+Preparation Time: 10 min
+
+Difficulty: intermediate
 
 Ingredients:
-- Mince
-- Mushroom
-- Pasta
+- 100 grams Cheese
+- 2 pieces Bread
+- 1 pieces Avocado
+- 20 grams Butter
+- 1 pieces Spicy Pepper
+
+Kitchen Tools Needed:
+- knife
+- cutting board
+- toaster
 
 Instructions:
-1. Put pasta in boiling water
-2. Fry mince and mushroom
+- Toast the bread slices in a toaster.
+- Spread butter on one side of each bread slice.
+- Slice the avocado and the spicy pepper.
+- Grate the cheese.
+- Layer the avocado, spicy pepper, and cheese on one bread slice.
+- Cover with the other bread slice.
+- Cut the sandwich diagonally.
+- Serve and enjoy!
+
+Macros:
+- Total Calories: 350kcal
+- Carbs: 30grams
+- Proteins: 15grams
+- Fats: 20grams
 `
 }
